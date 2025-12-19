5c015c87a35919f1cba653df6880a579a202eccf
OniOnDoodles <152353715+OniOnDoodles@users.noreply.github.com>
2025-12-19 15:13:06 +0700

    add: profile

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,5 +1,6 @@
-import { FlatList, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useAuth } from '@/lib/auth-provider';
 import { useIsFocused } from '@react-navigation/native';
+import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 
@@ -7,7 +8,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 export default function HomeScreen() {
 	const isFocused = useIsFocused()
 
-
+const {user} = useAuth()
 	// console.log(result);
 
 
@@ -16,9 +17,22 @@ export default function HomeScreen() {
 
 
 	return (
-		<SafeAreaView style={{ paddingHorizontal: 16 }}>
+		<SafeAreaView style={{ flex: 1, paddingHorizontal: 16 }}>
+					<View style={styles.container}>
+				<Text style={styles.title}>My Profile 💖</Text>
 
+				<View style={styles.card}>
+					<Text style={styles.label}>Full Name</Text>
+					<Text style={styles.value}>{user?.fullname}</Text>
 
+					<Text style={styles.label}>Username</Text>
+					<Text style={styles.value}>{user?.username}</Text>
+
+					<Text style={styles.label}>Role</Text>
+					<Text style={styles.value}>{user?.role}</Text>
+				</View>
+			</View>
+		
 
 
 		</SafeAreaView>
@@ -26,21 +40,34 @@ export default function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
-	titleContainer: {
-		flexDirection: 'row',
-		alignItems: 'center',
-
-		gap: 8,
-	},
-	stepContainer: {
-		gap: 8,
-		marginBottom: 8,
-	},
-	reactLogo: {
-		height: 178,
-		width: 290,
-		bottom: 0,
-		left: 0,
-		position: 'absolute',
-	},
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: "#fff",
+  },
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 24,
+		color: "black"
+  },
+  card: {
+    padding: 20,
+    borderRadius: 12,
+    backgroundColor: "#f5f5f5",
+  },
+  label: {
+    fontSize: 12,
+    color: "black",
+    marginTop: 12,
+  },
+  value: {
+    fontSize: 16,
+    fontWeight: "500",
+  },
 });
